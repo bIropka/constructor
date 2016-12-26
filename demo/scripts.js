41d81e8f818fd7ebb4724bf9461a2c85dd80316e
bIropka <biropka@gmail.com>
2016-12-27 00:09:20 +0200

    v 0.2

--- demo/scripts.js
+++ demo/scripts.js
@@ -1,9 +1,18 @@
 $(document).ready(function () {
 
+    /* nav */
     $('nav .fa-bars').click(function() {
         
         $(this).toggleClass('active');
         $(this).siblings('ul').slideToggle(200);
+
+    });
+
+    /* menu */
+    $('.menu .title').click(function() {
+
+        $(this).siblings('ul').slideToggle(200);
+        $(this).find('i').toggleClass('fa-arrow-down fa-arrow-up');
 
     });
 
@@ -21,6 +30,7 @@ $(document).ready(function () {
 
     });
 
+    /* menu-chbx */
     $('.form-menu-chbx .sub-title').click(function() {
 
         $(this).next('.fields').slideToggle(200);
@@ -28,11 +38,26 @@ $(document).ready(function () {
 
     });
 
+
     $('.form-menu-chbx .show-more').click(function() {
 
         $(this).siblings('.unpopular').slideToggle(200);
         $(this).toggleClass('active');
 
     });
+
+    $('.menu-chbx .title').click(function() {
+
+        $(this).siblings('.form-menu-chbx').addClass('active');
+
+    });
+
+    $('.form-menu-chbx > i').click(function() {
+
+        $(this).parents('.form-menu-chbx').removeClass('active');
+
+    });
+
+    /* */
 
 });
